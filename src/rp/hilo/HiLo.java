33c79438772d5e08670fdf309185a2e735b64bdc
trueprog <trueprogandy@gmail.com>
2017-02-01 17:55:22 +0100

    Reveal cards as buttons are pressed.

--- src/rp/hilo/HiLo.java
+++ src/rp/hilo/HiLo.java
@@ -5,6 +5,9 @@
  */
 package rp.hilo;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
@@ -21,17 +24,9 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-/**
- *
- * @author andwh
- */
 public class HiLo extends Application {
     
-    private StackPane root;
-    
-    public static void main(String[] args) {
-        launch();
-    }
+    private StackPane root;  
     private Image bgImage;
     private Image cardBack;
     private ImageView[] cv = new ImageView[2];
@@ -40,8 +35,18 @@ public class HiLo extends Application {
     private Button hiBtn;
     private Button loBtn;
     
+    private List<Integer> stack;
+    private int card1;
+    private int card2;
+    private boolean betHigh;
+
+    public static void main(String[] args) {
+        launch();
+    }
+    
     @Override
     public void start(Stage primaryStage) {
+        shuffleCardStack();
         root = new StackPane();
         root.getChildren().add(addBackground());
         BorderPane bp = new BorderPane();
@@ -51,6 +56,14 @@ public class HiLo extends Application {
         primaryStage.setTitle("HiLo Game");
         primaryStage.setScene(new Scene(root, bgImage.getWidth(), bgImage.getHeight()));
         primaryStage.show();
+    }
+
+    private void shuffleCardStack() {
+        stack = new ArrayList<Integer>();
+        for (int i = 0; i < 52; i++) {
+            stack.add(i);
+        }
+        Collections.shuffle(stack);
     }
     
     private ImageView addBackground() {
@@ -89,9 +102,15 @@ public class HiLo extends Application {
         });
         hiBtn = new Button("Bet on high");
         hiBtn.setMaxWidth(Double.MAX_VALUE);
+        hiBtn.setOnAction((ActionEvent e) -> {
+            this.betHigh();
+        });
         hiBtn.setDisable(true);
         loBtn = new Button("Bet on low");
         loBtn.setMaxWidth(Double.MAX_VALUE);
+        loBtn.setOnAction((ActionEvent e) -> {
+            this.betLow();
+        });
         loBtn.setDisable(true);
         VBox vb = new VBox(10, dealBtn, hiBtn, loBtn);
         vb.setPadding(new Insets(0, 10, 0, 10));
@@ -101,6 +120,27 @@ public class HiLo extends Application {
 
     private void dealCards() {
         dealBtn.setDisable(true);
+        card1 = stack.remove(0);
+        card2 = stack.remove(1);
+        showCard(0, card1);
+        hiBtn.setDisable(false);
+        loBtn.setDisable(false);
+    }
+
+    private void betHigh() {
+        betHigh = true;
+        processBet();
+    }
+
+    private void betLow() {
+        betHigh = false;
+        processBet();
     }
     
+    private void processBet() {
+        hiBtn.setDisable(true);
+        loBtn.setDisable(true);
+        showCard(1, card2);
+    }
+
 }
